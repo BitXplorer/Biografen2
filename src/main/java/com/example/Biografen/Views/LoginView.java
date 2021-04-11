4da2fde1b49b61e66faed6ffcbbb4bd3ed6bd545
MikaelNorrman <mikael.norrman@gmail.com>
2021-04-11 13:33:27 +0200

    Login and New pom dependencys

---
+++ src/main/java/com/example/Biografen/Views/LoginView.java
@@ -0,0 +1,36 @@
+package com.example.Biografen.Views;
+
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("login")
+@PageTitle("Login - Newton Cinema")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+    private LoginForm loginForm = new LoginForm();
+
+
+    public LoginView() {
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        loginForm.setAction("login");
+
+        add(new H1("Newton Cinema"), loginForm);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        if (beforeEnterEvent.getLocation().getQueryParameters().getParameters().containsKey("error")) {
+            loginForm.setEnabled(true);
+        }
+    }
+}
