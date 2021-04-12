3541b089bc274944e2c6d1ce2438d420ff9f65ac
MikaelNorrman <mikael.norrman@gmail.com>
2021-04-12 14:27:10 +0200

    Turning a Vaadin Flow Application Into an Installable PWA. Customize the Offline Page

--- src/main/java/com/example/Biografen/BiografenApplication.java
+++ src/main/java/com/example/Biografen/BiografenApplication.java
@@ -1,5 +1,7 @@
 package com.example.Biografen;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
@@ -7,8 +9,13 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class BiografenApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BiografenApplication.class);
+
 	public static void main(String[] args) {
+
+		log.info("STARTING : Spring boot application starting");
 		SpringApplication.run(BiografenApplication.class, args);
+		log.info("STOPPED  : Spring boot application stopped");
 	}
 
 }
